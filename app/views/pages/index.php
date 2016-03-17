7f72682b91aafaf5cd2ce1df4e27d015d13e4edb
pablogabolde <pablo69580@gmail.com>
2016-03-17 10:29:26 +0100

    Ajout de A propos

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,2 +1,20 @@
-<p>La vue des pages index, avec une variable quelconque passé a la vue : <?php echo $nom ?>.</p>
+<div class="content">
+    <center><h1> Bienvenue sur How to Code !</h1> </center>
+</div><br/>
+
+<div class="content">
+    <div class="col-xs-4">
+    <p> Ce site, développé par GODIN Pascal, a pour but de former les passionés dinformatique aux domaines qui les intéresse. </p>
+    </div>
+    
+    <div class="col-xs-4">
+        <p> Les tutoriels sont réalisés de façon à ce que l'utilisateur comprenne les choses facilement. </p>
+    </div>
+
+    <div class="col-xs-4">
+        <p> La méthode de travail pour développer ce site est basé sur les méthodes agiles, avec comme Scrum Master Monsieur MICHUT, accompagné par sa Team Dev' composée de GABOLDE Pablo, PETRINI Amaury et [insérer un nom] Cédric.  </p>
+    </div>
+</div>
+
+<img src=""/>
 
