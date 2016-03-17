d3dfcd2bbea2189cf677f064ca7d65f00f92020f
oudirichi <godin_boy@msn.com>
2016-03-17 10:59:03 +0100

    Adding the possibility to export a tutoriel to pdf

--- app/views/tutoriel/view_guide.php
+++ app/views/tutoriel/view_guide.php
@@ -14,7 +14,7 @@ $modif = $tutoriel['modification'];
 
 
 
-<div class="content-section-b">
+<div class="content-section-b" id="content">
 
     <div class="row">
 
@@ -24,5 +24,30 @@ $modif = $tutoriel['modification'];
 
     </div>
 
+	</div>
+	<div id="editor"><button id="cmd">generate PDF</button></div>
 </div>
+		<?php ob_start(); ?>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jspdf/0.9.0rc1/jspdf.min.js"></script>
+<script>
+var doc = new jsPDF();
+var specialElementHandlers = {
+    '#editor': function (element, renderer) {
+        return true;
+    }
+};
+
+$('#cmd').click(function () {
+    doc.fromHTML($('#content').html(), 15, 15, {
+        'width': 170,
+            'elementHandlers': specialElementHandlers
+    });
+    doc.save('<?= $tutoriel['title']; ?>.pdf');
+});
+
+</script>
+
+		<?php $script = ob_get_clean(); ?>
+
 <!-- /.content-section-a -->
